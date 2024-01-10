c3463dabefded0724bb9af84de1762e8af79b165
Pawel Krol <paullkroll@icloud.com>
2024-01-10 23:28:23 +0100

    get, post, put all works - delete 404 not found

--- server.js
+++ server.js
@@ -5,59 +5,64 @@ const db = require('./public/db');
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-app.get('/testimonials', (req, res) => {
+app.get('/testimonials', (req, res) => { // ok1
     res.json({ testimonials: db });
 });
 
-app.post('/testimonials', (req, res) => {
+app.get('/testimonials/random', (req, res) => {  // ok2 
+    const random = Math.floor(Math.random() * db.length);
+    const randomTestimonial = db[random];
+
+    res.json({ randomTestimonial });
+});
+
+app.get('/testimonials/:id', (req, res) => { // ok3 
+    const id = parseInt(req.params.id);
+    const activeId = db.filter(item => item.id === id);
+    console.log(activeId)
+
+    res.json({ activeId });
+});
+
+app.post('/testimonials', (req, res) => { // ok4
     const randomId = uuid.v4();
     const { author, text } = req.body;
     if(author && text ) {
         const newTest = { id: randomId, author, text }
-        res.json( newTest, { message: 'OK' });
+        res.json( { newTest, message: 'OK' });
       }
       else {
         res.json({ message: 'Something went wrong!'});
       }
 });
 
-app.put('/testimonials/:id', (req, res) => {
+app.put('/testimonials/:id', (req, res) => { //ok5
     const { author, text } = req.body;
     const id = req.params.id;
-    const activeTest = db.find(item => item.id === id);
+    const activeTest = db.filter(item => item.id === id);
     if (activeTest) {
         activeTest.author = author;
         activeTest.text = text;
-        res.json({ message: 'Testimonial updated', activeTest });
+        res.json({ message: 'Testimonial updated'});
     } else {
         res.json({ message: 'Testimonial not found' });
     }
 });
 
 app.delete('/testimonials/:id'), (req, res) => {
-    const id = req.params.id;
-    const testToDelete = db.findIndex(item => item.id === id);
-    const deleteTest = db.splice(testToDelete, 1);
-    res.json({ message: 'OK',  deleteTest });
+    const id = parseInt(req.params.id);
+    const testToDelete = db.find(item => item.id === id);
+    if(testToDelete){
+        db = db.filter(item => item.id !== id);
+        res.json({ message: 'OK' , deletedTestimonial: testToDelete});
+    }else {
+        res.json({ message: 'Something wrong'});
+    }
 };
 
-app.get('/testimonials/:id', (req, res) => {
-    const id = req.params.id;
-    const activeId = db.find(item => item.id === id);
-
-    res.json({ activeId });
-});
-
-app.get('/testimonials/random', (req, res) => {
-    const random = Math.floor(Math.random() * db.length);
-    console.log('random', random);
-    const randomTestimonial = db[random];
-
-    res.json({ randomTestimonial });
-});
-
-app.use((req, res) => {
+app.use((req, res) => { // ok7
     res.status(404).send({ message: '404 not found...' });
 });
 
