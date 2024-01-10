1d01be196ffb9dfcb6a474ebb3fd6dba20cb5241
Pawel Krol <paullkroll@icloud.com>
2024-01-10 15:25:52 +0100

    Add get for 3 endpoints and add put for testimony, add uuid package

--- server.js
+++ server.js
@@ -1,9 +1,38 @@
 const express = require('express');
+const db = require('./public/db');
+const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json(path.join(__dirname, '/public')));
+
+app.get('/testimonials', (req, res) => {
+    res.json('testimonials', { testimonials: db });
+});
+
+app.post('/testimonials', (req, res) => {
+    const { author, text, id } = req.body;
+    if(author && text && id) {
+        res.json('contact', { message: 'OK' });
+      }
+      else {
+        res.json('contact', { message: 'Something went wrong!' });
+      }
+});
+
+app.get('/testimonials/:id', (req, res) => {
+    const id = req.params.id;
+    const activeId = db.find(item => item.id === id);
+
+    res.json('testimonials/:id', { activeId });
+});
+
+app.get('/testimonials/random', (req, res) => {
+    const random = Math.floor(Math.random() * db.length);
+    const randomTestimonial = db[random];
+
+    res.json('testimonials/random', { randomTestimonial });
 });
 
 app.listen(8000, () => {
